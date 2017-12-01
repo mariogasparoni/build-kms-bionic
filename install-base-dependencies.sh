9c8178450daba0a1f0ce4a7ea542dfd83ddb1b7e
Mario Jr <mariogasparoni@gmail.com>
2017-12-01 14:30:36 -0200

    Base dependencies for kurento media server

---
+++ install-base-dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install --no-install-recommends -y build-essential cmake debhelper\
+  binutils pkg-config libboost-dev libboost-regex-dev libboost-system-dev\
+  libboost-filesystem-dev libboost-program-options-dev libboost-test-dev\
+  libboost-thread-dev libboost-log-dev libevent-dev libssl-dev libboost-all-dev\
+  git openjdk-8-jdk maven libsigc++-2.0-dev libglibmm-2.4-dev uuid-dev\
+  libgnutls-dev libvpx-dev wget
