0dc93e54033223cadb41f18742da9918473c9936
Mario Jr <mariogasparoni@gmail.com>
2018-01-29 14:51:16 -0200

    Fix path for kurento-module-creator
    This might become a patch for kurento

--- build-kurento-module-creator.sh
+++ build-kurento-module-creator.sh
@@ -7,14 +7,14 @@ KURENTO_MODULE_CREATOR_DIR=$ROOT/kurento-module-creator
 cd $KURENTO_MODULE_CREATOR_DIR
 mvn package -DskipTests -DbuildDirectory=$KURENTO_MODULE_CREATOR_DIR
 
-cp scripts/kurento-module-creator .
-touch kurento-module-creator.generated
 
 #Without the following replacement, other modules will use the wrong
 #path when looking for this module
 #this should be a patch, i guess
-sed -i 's/PATH_SUFFIXES//g' src/main/cmake/FindKurentoModuleCreator.cmake
-sed -i 's/scripts//g' src/main/cmake/FindKurentoModuleCreator.cmake
+sed -i 's/JAVA_JAR=${MY_PATH}\/..\/target/JAVA_JAR=$(dirname ${MY_PATH})/g' scripts/kurento-module-creator
+
+cp scripts/kurento-module-creator .
+touch kurento-module-creator.generated
 
 #Change template for correct path when loooking for cmake modules locally
 #this should set correct path for finder files (FindYYY.cmake file)
