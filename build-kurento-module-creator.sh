9681deb7ec27ad3c63bbe3ffb0e6b6e202be6504
Mario Jr <mariogasparoni@gmail.com>
2018-02-05 19:08:09 -0200

    Added back replacements for Kurento Module Creator
    Without this, other modules can't find KMSCORE

--- build-kurento-module-creator.sh
+++ build-kurento-module-creator.sh
@@ -8,9 +8,11 @@ cd $KURENTO_MODULE_CREATOR_DIR
 mvn package -DskipTests -DbuildDirectory=$KURENTO_MODULE_CREATOR_DIR
 
 
-#Without the following replacement, other modules will use the wrong
+#Without the following replacements, other modules will use the wrong
 #path when looking for this module
 #this should be a patch, i guess
+sed -i 's/PATH_SUFFIXES//g' src/main/cmake/FindKurentoModuleCreator.cmake
+sed -i 's/scripts//g' src/main/cmake/FindKurentoModuleCreator.cmake
 sed -i 's/JAVA_JAR=${MY_PATH}\/..\/target/JAVA_JAR=$(dirname ${MY_PATH})/g' scripts/kurento-module-creator
 
 cp scripts/kurento-module-creator .
